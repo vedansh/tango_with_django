a74ddb8ca97f63ca5cc5b730ee88473ee7b482ab
vedansh <vedanshkt.sfc@gmail.com>
2015-04-01 22:30:21 +0530

    6.populate db

--- rango/views.py
+++ rango/views.py
@@ -2,18 +2,12 @@
 from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
+from rango.models import Category
 
-#def index(request):
-#    return HttpResponse("Rango says hey there world!")
 
 def index(request):
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
-
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-
+    # Render the response and send it back!
     return render(request, 'rango/index.html', context_dict)
